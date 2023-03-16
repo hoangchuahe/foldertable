35f8e9044b90430059a3a6cb02c226d9b01d9f39
Phạm Nhật Hoàng <medigo.hoang.pham@gmail.com>
2023-03-16 13:57:03 +0700

    new

--- src/app/folder-table/folder-table/folder-table.component.ts
+++ src/app/folder-table/folder-table/folder-table.component.ts
@@ -13,65 +13,89 @@ import { FilterPipe } from '../service/search';
   styleUrls: ['./folder-table.component.scss']
 })
 export class FolderTableComponent {
-  searchText = '';
-
+  searchText:string;
+  changeCount: number = 0;
   inSearchMode =false;
   public lstPath: any[] = ['.'];
-  files: File[]=[];
-  result:string;
+  showList: File[]=[];
+  tempList:any[]=[]
+  searchList:any[]=[]
+  ogiList:any[]=[]
+  currList:any=[]
   constructor(private folderService: FolderService,){}
   public searchValue: string = null;
   public handleKeyupSearch$ = new Subject<any>();
 
-  updateRecentlyPath(index: number) {
-    // this.isLoading = true;
-    this.lstPath.splice(index + 1, this.lstPath.length - index - 1);
-    this.getFiles();
-    this.findValue(this.files,this.lstPath[this.lstPath.length-1])
-  }
-
-
-resetSearchInput() {
-  this.searchValue = '';
-
-  this.handleKeyupSearch$.next('');
-}
-
- findValue(arr:any, val:string):void  {
-  for(let obj of arr){
-    if(obj.isFolder){
-      if(obj.name === val){
-        this.files=obj.children;
-      }else{
-        this.findValue(obj.children,val)
-      }
-    }
-  }
-}
-
-
-
-
-
-
-// Outputs: { id: 4, name: "Rahul" }
-
-
-  navigate(folder:File) {
-    if (folder.isFolder) {
-      this.files=folder.children;
-      this.lstPath.push(folder.name);
-    }
-  }
   ngOnInit(): void {
     this.getFiles();
+    this.ogiList=this.showList
+    this.currList=this.showList
   }
 
 
   getFiles(): void {
     this.folderService.getFiles()
-      .subscribe(files => this.files =files)
+      .subscribe(files => this.showList =files)
   }
 
+  updateRecentlyPath(index: number) {
+    this.lstPath.splice(index + 1, this.lstPath.length - index - 1);
+    this.getFiles();
+    this.callBack(this.showList,this.lstPath[this.lstPath.length-1])
+  }
+
+
+resetSearchInput() {
+  this.searchValue = '';
+  this.handleKeyupSearch$.next('');
+}
+
+ callBack(arr:any, val:string):void  {
+  for(let obj of arr){
+    if(obj.isFolder){
+      if(obj.name === val){
+        this.showList=obj.children
+        this.currList=this.showList
+      }else{
+        this.callBack(obj.children,val)
+      }
+    }
+  }
+}
+
+  navigate(folder:any) {
+    if (folder.isFolder) {
+      this.showList=folder.children;
+      this.currList = this.showList
+      this.lstPath.push(folder.name);
+    }
+  }
+
+  getAllFilesInFolders(listTemp:any) {
+    for (let i = 0; i < listTemp.length; i++) {
+      this.searchList.push(listTemp[i]);
+      const temp = listTemp[i].children;
+      if (temp) {
+        this.getAllFilesInFolders(temp);
+      }
+    }
+  }
+
+  search(value:string){
+    this.tempList=[]
+    this.getAllFilesInFolders(this.currList)
+    if(value===''||value===null){
+      this.inSearchMode =false
+      this.showList=this.currList
+    }else{
+      this.inSearchMode=true
+      for(let obj of this.searchList){
+        if(obj.name.includes(value)){
+          this.tempList.push(obj)
+        }
+      }
+      this.showList=this.tempList
+    }
+  }
 
 }
